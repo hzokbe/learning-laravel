6f081deabbe1319e222d91b0085c146acb86b8ef
hzokbe <hzokbe@gmail.com>
2026-09-21 17:54:04 -0300

    routes: receiving more parameters

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,6 @@ Route::get('/', [HomeController::class, 'index']);
 
 Route::get('/about', [AboutController::class, 'index']);
 
-Route::get('/hello/{name}', function (string $name) {
-    return '<h1>Hello, ' . $name . '!<h1>';
-});
+Route::get('/hello/{name}/{age}', function (string $name, int $age) {
+    return "<h1>Hello, $name!<h1>\n<p>Your age is $age</p>";
+})->whereNumber('age');
