bab96c1198e3830d9b2b74adbec49f95cc5b4b3c
hzokbe <hzokbe@gmail.com>
2026-09-22 16:06:23 -0300

    views: giving parameters to views

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -2,11 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class HelloController extends Controller
 {
-    public function index(string $name, string $last_name) {
-        return "Hello, $name $last_name!";
+    public function index(string $name, string $last_name)
+    {
+        return view('hello')->with('name', $name)->with('last_name', $last_name);
     }
 }
