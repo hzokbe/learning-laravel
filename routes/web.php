5b63e4a42f363a59d82693174859a4bde9897420
hzokbe <hzokbe@gmail.com>
2026-09-22 10:10:19 -0300

    routes: defining more routes

--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,17 @@ Route::get('/about', [AboutController::class, 'index']);
 Route::get('/hello/{name}/{age}', function (string $name, int $age) {
     return "<h1>Hello, $name!<h1>\n<p>Your age is $age</p>";
 })->where('name', '[A-Za-z]+')->whereNumber('age');
+
+Route::get('/login', function () {
+    return 'Login!';
+});
+
+Route::prefix('/app')->group(function () {
+    Route::get('/products', function () {
+        return 'Products!';
+    });
+
+    Route::get('/customers', function () {
+        return 'Customers!';
+    });
+});
